44ceab2ca4d031e47c7550c24c95c6081efd6a89
Chen Rabiner <rabiner@gmail.com>
2022-09-27 01:35:38 +0300

    sdl2 display vidoe

--- display.py
+++ display.py
@@ -1,12 +1,27 @@
 #!/usr/bin/env python3
 import cv2
-W = 1920 // 2
-H = 1080 // 2
+import sdl2.ext
+
+# sdl2 requires init
+sdl2.ext.init()
+
+WIDTH = 1920 // 2
+HEIGHT = 1080 // 2
+
+window = sdl2.ext.Window("SLAM window", size=(WIDTH, HEIGHT), position=(0, 0))
+window.show()
 
 def process_frame(img):
-    img = cv2.resize(img, (W, H))
-    cv2.imshow('image', img)
-    cv2.waitKey()
+    img = cv2.resize(img, (WIDTH, HEIGHT))
+    events = sdl2.ext.get_events()
+    for event in events:
+        if event.type == sdl2.SDL_KEYDOWN:
+            exit(0)
+        if event.type == sdl2.SDL_QUIT:
+            exit(0)
+    surf = sdl2.ext.pixels3d((window.get_surface()))
+    surf[:, :, 0:3] = img.swapaxes(0,1)
+    window.refresh()
     print(img.shape)
 
 
