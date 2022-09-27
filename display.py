b52dfa3113a563c55636243b4b586426e7f5a767
Chen Rabiner <rabiner@gmail.com>
2022-09-27 21:34:43 +0300

    Can see orb keypoints detected on tree tops, but they are not well distributed

--- display.py
+++ display.py
@@ -13,6 +13,7 @@ class Display (object):
     def __init__(self, W, H):
         self.window = sdl2.ext.Window("SLAM window", size=(WIDTH, HEIGHT), position=(0, 0))
         self.window.show()
+        self.frame = 0
 
     def paint(self, img):
         events = sdl2.ext.get_events()
@@ -22,7 +23,10 @@ class Display (object):
             if event.type == sdl2.SDL_QUIT:
                 exit(0)
 
+        cv2.putText(img=img, text=str(self.frame), org=(10, 30), fontFace=cv2.FONT_HERSHEY_TRIPLEX, fontScale=1,
+                    color=(0, 215, 0), thickness=1, lineType=cv2.LINE_AA)
         surf = sdl2.ext.pixels3d((self.window.get_surface()))
         surf[:, :, 0:3] = img.swapaxes(0,1)
         self.window.refresh()
+        self.frame += 1
 
--- slam.py
+++ slam.py
@@ -4,15 +4,20 @@ from display import Display
 
 WIDTH = 1920 // 2
 HEIGHT = 1080 // 2
+MAX_FEATURES = 700
 
 disp = Display(WIDTH, HEIGHT)
-
+orb = cv2.ORB_create(MAX_FEATURES)
+# features track point from one image to the next image
 def process_frame(img):
+    keypoints, descriptors = orb.detectAndCompute(img, None)
+
+    for point in keypoints:
+        u, v = map(lambda x: int(round(x)), point.pt)
+        cv2.circle(img, (u, v), color=(0, 255, 0), radius=3)
+
     img = cv2.resize(img, (WIDTH, HEIGHT))
     disp.paint(img)
-    # print(img.shape)
-
-
 
 if __name__ == "__main__":
     cap = cv2.VideoCapture("videos/test_countryroad.mp4")
