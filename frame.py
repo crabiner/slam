2d2d91b9ab87cc46563597e88f30dc1a7af8a49f
Chen Rabiner <rabiner@gmail.com>
2022-10-01 21:00:19 +0300

    refactor to frame

---
+++ frame.py
@@ -0,0 +1,114 @@
+#!/usr/bin/env python3
+import cv2
+import numpy as np
+np.set_printoptions(suppress=True)
+
+from skimage.measure import ransac
+from skimage.transform import FundamentalMatrixTransform
+from skimage.transform import EssentialMatrixTransform
+
+# turn [[x,y]] -> [[x,y,1]]
+def add_ones(x):
+    return np.concatenate([x, np.ones((x.shape[0], 1))], axis=1)
+
+
+def extractRt(E):
+    W = np.mat([[0, -1, 0], [1, 0, 0], [0, 0, 1]], dtype=float)
+    U, d, Vt = np.linalg.svd(E)
+    assert np.linalg.det(U) > 0
+    if np.linalg.det(Vt) < 0:
+        Vt *= -1.0
+    R = np.dot(np.dot(U, W), Vt)
+    if np.sum(R.diagonal()) < 0:
+        R = np.dot(np.dot(U, W.T), Vt)
+    t = U[:, 2]
+    Rt = np.concatenate([R, t.reshape(3, 1)], axis=1)
+    return Rt
+
+def extract(img):
+    # detection
+    orb = cv2.ORB_create()
+
+    # use opencv good features to track
+    points = cv2.goodFeaturesToTrack(np.mean(img, axis=2).astype(np.uint8),
+                                     3000,
+                                     qualityLevel=0.01,
+                                     minDistance=3)
+
+    # extraction
+    keypoints = [cv2.KeyPoint(x=f[0][0], y=f[0][1], size=20) for f in points]
+    keypoints, descriptors = orb.compute(img, keypoints)
+
+    # return pts and des
+    return np.array([(kp.pt[0], kp.pt[1]) for kp in keypoints]), descriptors
+
+
+def normalize(Kinv, pts):
+    return np.dot(Kinv, add_ones(pts).T).T[:, 0:2]
+
+# use the projection matrix of the camera
+def denormalize(K, pt):
+    # https://docs.opencv.org/4.x/d9/d0c/group__calib3d.html
+    # Take the homogenous coordinates and left multiply it by the K matrix
+    ret = np.dot(K, np.array([pt[0], pt[1], 1.0]))
+    ret /= ret[2]
+    return int(round(ret[0])), int(round(ret[1]))
+
+
+
+def match(f1, f2):
+    # matching
+
+    # Brute-Force matcher is simple. It takes the descriptor of one feature in first set and is matched
+    # with all other features in second set using some distance calculation. And the closest one is returned.
+    bf = cv2.BFMatcher(cv2.NORM_HAMMING)
+
+    # match to previous frame using brute force matcher
+    matches = bf.knnMatch(f1.descriptors, f2.descriptors, k=2)
+
+    # Lowe's ratio test
+    ret = []
+    for m, n in matches:
+        if m.distance < 0.75 * n.distance:
+            p1 = f1.points[m.queryIdx]
+            p2 = f2.points[m.trainIdx]
+            ret.append((p1, p2))
+    assert len(ret) >= 8
+    ret = np.array(ret)
+
+    # normalize coordinates: subtract to move to zero (lens image center)
+    # ret[:, 0, :] = normalize(ret[:, 0, :])
+    # ret[:, 1, :] = normalize(ret[:, 1, :])
+
+    # filter
+    Rt = None
+
+    # fit matrix
+    # When we have the F number (calibrated camera) we can use
+    # EssentialMatrixTransform and it estimates less parameters
+    model, inliers = ransac((ret[:, 0], ret[:, 1]),
+                            EssentialMatrixTransform,
+                            # FundamentalMatrixTransform,
+                            min_samples=8,
+                            residual_threshold=0.005,  # lower residual threshold to get less errors
+                            max_trials=200)
+
+    # ignore outliers
+    # now we want just the inliers and not the noise
+    ret = ret[inliers]
+
+    # extract rotation and translation
+    Rt = extractRt(model.params)
+
+    # we only care about the matches
+    return ret, Rt
+
+
+class Frame(object):
+    def __init__(self, img, K):
+        self.K = K
+        self.Kinv = np.linalg.inv(self.K)
+
+        pts, self.descriptors = extract(img)
+        self.points = normalize(self.Kinv, pts)
+
--- slam.py
+++ slam.py
@@ -2,36 +2,39 @@
 import cv2
 from display import Display
 import numpy as np
-from extractor import Extractor
+from frame import Frame, denormalize, match
+import g2o
 
+# camera intrinsics
 WIDTH = 1920 // 2
 HEIGHT = 1080 // 2
 
-disp = Display(WIDTH, HEIGHT)
-
-F = 270
-# principal point that is usually at the image center
 cx = WIDTH // 2
 cy = HEIGHT // 2
-
+F = 270
 K = np.array([[F, 0, cx], [0, F, cy], [0, 0, 1]])
+# principal point that is usually at the image center
 
-fe = Extractor(K)
+# main classes
+disp = Display(WIDTH, HEIGHT)
 
-
+frames = []
 def process_frame(img):
     img = cv2.resize(img, (WIDTH, HEIGHT))
-    matches, pose = fe.extract(img)
 
-    if pose is None:
+    frame = Frame(img, K)
+    frames.append(frame)
+    if len(frames) <= 1:
         return
 
-    print("%d matches" % (len(matches)))
-    print(pose)
+    ret, Rt = match(frames[-1], frames[-2])
 
-    for pt1, pt2 in matches:
-        u1, v1 = fe.denormalize(pt1)
-        u2, v2 = fe.denormalize(pt2)
+    # print("%d matches" % (len(matches)))
+    # print(pose)
+
+    for pt1, pt2 in ret:
+        u1, v1 = denormalize(K, pt1)
+        u2, v2 = denormalize(K, pt2)
 
         cv2.circle(img, (u1, v1), color=(0, 255, 0), radius=3)
         cv2.line(img, (u1, v1), (u2, v2), color=(255, 0, 0))
